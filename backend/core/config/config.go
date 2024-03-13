c94a7ec5e0ea514d240ea929a1044f6efc2df806
devgpt-ai <endaqi@jiewatech.com>
2024-03-13 07:23:02 +0000

    Task description

--- backend/core/config/config.go
+++ backend/core/config/config.go
@@ -21,20 +21,20 @@ import "time"
 
 // ConfigReader defined a serial of interfaces
 type ConfigReader interface {
-	Get(key string) interface{}
-	GetBool(name string) bool
-	GetFloat64(key string) float64
-	GetInt(key string) int
-	GetInt64(key string) int64
-	GetUint(key string) uint
-	GetUint64(key string) uint64
-	GetIntSlice(key string) []int
-	GetString(key string) string
-	GetStringMap(key string) map[string]interface{}
-	GetStringMapString(key string) map[string]string
-	GetStringSlice(key string) []string
-	GetTime(key string) time.Time
-	GetDuration(key string) time.Duration
-	IsSet(key string) bool
-	AllSettings() map[string]interface{}
+Get(key string) interface{}
+GetBool(name string) bool
+GetFloat64(key string) float64
+GetInt(key string) int
+GetInt64(key string) int64
+GetUint(key string) uint
+GetUint64(key string) uint64
+GetIntSlice(key string) []int
+GetString(key string) string
+GetStringMap(key string) map[string]interface{}
+GetStringMapString(key string) map[string]string
+GetStringSlice(key string) []string
+GetTime(key string) time.Time
+GetDuration(key string) time.Duration
+IsSet(key string) bool
+AllSettings() map[string]interface{}
 }
